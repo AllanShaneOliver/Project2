3aac76445f0f51a96acf8dfeff4bb5d2f98df615
Shane Oliver <shaneoliver@Shanes-Air.fios-router.home>
2020-11-06 23:49:54 -0500

    Initializes the string which the user input will be stored into

---
+++ project2.s
@@ -0,0 +1,11 @@
+.data
+invalid: .asciiz "Invalid Input!\n"
+list: .space 1002 #can store 1000 characters into a string named "list"
+.text
+.globl main
+
+main:
+  li $v0, 8 #reads strings into list string
+  la $a0, list
+  li $a1, 1001 #input no more than 1001 characters
+  
